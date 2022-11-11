774e1672cece379300f77ce8e45f8b46a7ccba69
YashJain24 <75324135+YashJain24-chief@users.noreply.github.com>
2022-11-11 11:04:12 +0530

    refactored code

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from './screens/SplashScreen';
